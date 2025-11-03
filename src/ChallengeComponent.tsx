e77389b484da9b69fec83f06a45a575da8853997
Andrew Chae <asjchae@gmail.com>
2025-11-02 22:49:21 -0800

    Add task button

--- src/ChallengeComponent.tsx
+++ src/ChallengeComponent.tsx
@@ -4,11 +4,30 @@ import { useState } from 'react';
   
 export function ChallengeComponent() {
   const [tasks, setTasks] = useState<string[][]>(INITIAL_TASKS);
+  const [newTask, setNewTask] = useState<string>('');
+  const handleAddTask = () => {
+    if (newTask.trim() === '') {
+      return;
+    }
+    const newTasks = [...tasks];
+    newTasks[0].push(newTask);
+    setTasks(newTasks);
+    setNewTask('');
+  };
+
   return (
-    <div className="flex flex-row p-4">
-      {CATEGORIES.map((category, index) => (
-        <CategoryComponent key={category} category={category} tasks={tasks[index] || []} />
-      ))}
-    </div>
+    <>
+      <div className="flex flex-row p-4">
+        {CATEGORIES.map((category, index) => (
+          <CategoryComponent key={category} category={category} tasks={tasks[index] || []} />
+        ))}
+      </div>
+      <div className='flex flex-row p-4 m-2 gap-2'>
+        <input type="text" placeholder="Add task" value={newTask} onChange={(e) => setNewTask(e.target.value)} />
+        <button className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition-colors inline-block" onClick={handleAddTask}>
+          +
+        </button>
+      </div>
+    </>
   );
 }
